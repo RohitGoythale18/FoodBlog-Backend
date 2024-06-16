e198a2c00cadb41b41856e4575049477959d06fd
Rohit Goythale <rohitgoythale23@gmail.com>
2024-06-16 20:51:33 +0530

    Refactor: index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors({
